08741ba4c909b50b1fec8c69480229d5a15befe0
Safwat Fathi <safwat.rashwan@gmail.com>
2025-04-07 07:32:00 +0200

    update: notifiactions entity to have sentiment and classification columns

---
+++ src/migrations/1744003528730-UpdateNotificationForSentimentAndClassification.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class UpdateNotificationForSentimentAndClassification1744003528730
+  implements MigrationInterface
+{
+  name = 'UpdateNotificationForSentimentAndClassification1744003528730';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "notifications" ADD "sentiment" character varying`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "notifications" ADD "classification" character varying`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "notifications" DROP COLUMN "classification"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "notifications" DROP COLUMN "sentiment"`,
+    );
+  }
+}
